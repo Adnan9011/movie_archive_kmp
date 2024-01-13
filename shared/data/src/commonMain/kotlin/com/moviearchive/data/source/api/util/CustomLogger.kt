5dc45a3ab0cbc8effa5d9f9e69dcaf571a83afb9
Adnan9011 <adnan9011@gmail.com>
2024-01-13 17:19:52 +0330

    feat: Implement napier for logging

--- shared/data/src/commonMain/kotlin/com/moviearchive/data/source/api/util/CustomLogger.kt
+++ shared/data/src/commonMain/kotlin/com/moviearchive/data/source/api/util/CustomLogger.kt
@@ -1,10 +1,10 @@
 package com.moviearchive.data.source.api.util
 
+import io.github.aakira.napier.Napier
 import io.ktor.client.plugins.logging.Logger
 
 class CustomHttpLogger() : Logger {
     override fun log(message: String) {
-        //TODO: Fix Logging in Ktor
-//        Log.d("Ktor", message)
+        Napier.d(tag = "Ktor") { message }
     }
 }

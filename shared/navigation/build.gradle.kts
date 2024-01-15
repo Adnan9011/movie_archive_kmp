300ea6a9347ad19897ddd589c81012de00568477
Adnan9011 <adnan9011@gmail.com>
2024-01-15 15:36:42 +0330

    feat: Add navigation module

---
+++ shared/navigation/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+}
+
+kotlin {
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "navigation"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                projects.shared.apply {
+                    implementation(data)
+                    implementation(domain)
+                    implementation(feature)
+                }
+
+                libs.koin.apply {
+                    implementation(core)
+                }
+            }
+        }
+    }
+}
+
+android {
+    namespace = "com.moviearchive.navigation"
+    compileSdk = 34
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        jvmToolchain(17)
+    }
+}

3ebd42e1e15537dfa9ec05eefae03ef8d39c6c7a
Adnan9011 <adnan9011@gmail.com>
2024-01-17 20:24:24 +0330

    refactor: Improve ui detail screen

--- shared/feature/src/androidMain/kotlin/com/moviearchive/feature/android/presentation/detail/DetailScreen.kt
+++ shared/feature/src/androidMain/kotlin/com/moviearchive/feature/android/presentation/detail/DetailScreen.kt
@@ -3,7 +3,10 @@ package com.moviearchive.feature.android.presentation.detail
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.moviearchive.feature.model.MovieUiModel
 import com.moviearchive.feature.presentation.detail.DetailScreen
+import com.moviearchive.feature.presentation.detail.Header
+import org.koin.compose.koinInject
 
 @Preview
 @Composable
@@ -12,5 +15,21 @@ private fun DetailScreenPreview() {
         modifier = Modifier,
         movieId = 0,
         onBackClicked = {}
+    )
+}
+
+@Preview
+@Composable
+private fun HeaderPreview() {
+    Header(
+        movie = MovieUiModel(
+            id = 0,
+            imageUrl = "",
+            title = "Title",
+            numComments = 0,
+            numLikes = 0,
+            isLiked = false
+        ),
+        viewModel = koinInject()
     )
 }

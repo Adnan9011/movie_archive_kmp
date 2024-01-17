7080ee71656ba9f3bad202f99d508faa0398b81c
Adnan9011 <adnan9011@gmail.com>
2024-01-17 20:24:10 +0330

    refactor: Improve ui movie item

--- shared/feature/src/commonMain/kotlin/com/moviearchive/feature/presentation/home/MovieItem.kt
+++ shared/feature/src/commonMain/kotlin/com/moviearchive/feature/presentation/home/MovieItem.kt
@@ -3,7 +3,9 @@ package com.moviearchive.feature.presentation.home
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -19,6 +21,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -62,83 +65,93 @@ fun MovieItem(
                 .wrapContentSize()
                 .clickable { onShowDetail(movie.id) }
         ) {
-            Image(
+            Box(
                 modifier = Modifier
-                    .size(width = MovieItemWidth, height = MovieItemHeight),
-                painter = rememberAsyncImagePainter(
-                    model = ImageRequest.Builder(LocalPlatformContext.current).data(movie.imageUrl)
-                        .build()
-                ),
-                contentScale = ContentScale.Crop,
-                contentDescription = null
-            )
-            VerticalGradiant(
-                modifier = Modifier
-                    .width(MovieItemWidth),
-                listColors = listOf(
-                    GradientWhite,
-                    GradientDarkGray,
-                    GradientBlack
-                )
+                    .size(width = MovieItemWidth, height = MovieItemHeight)
             ) {
-                Text(
-                    text = movie.title,
-                    style = MovieItemTitleStyle,
+                Icon(
+                    if (movie.isLiked) Icons.Filled.Favorite else Icons.Filled.FavoriteBorder,
+                    tint = Color.Red,
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(
-                            top = SmallPadding,
-                            bottom = SmallPadding,
-                            start = NormalPadding,
-                            end = NormalPadding
-                        ),
-                    overflow = TextOverflow.Ellipsis,
-                    maxLines = 1
+                        .padding(NormalPadding)
+                        .align(Alignment.TopStart),
+                    contentDescription = null
                 )
+
+                Image(
+                    modifier = Modifier
+                        .size(width = MovieItemWidth, height = MovieItemHeight),
+                    painter = rememberAsyncImagePainter(
+                        model = ImageRequest.Builder(LocalPlatformContext.current)
+                            .data(movie.imageUrl)
+                            .build()
+                    ),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = null
+                )
+                VerticalGradiant(
+                    modifier = Modifier
+                        .width(MovieItemWidth)
+                        .align(Alignment.BottomStart),
+                    listColors = listOf(
+                        GradientWhite,
+                        GradientDarkGray,
+                        GradientBlack
+                    )
+                ) {
+                    Text(
+                        text = movie.title,
+                        style = MovieItemTitleStyle,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(
+                                top = SmallPadding,
+                                bottom = SmallPadding,
+                                start = NormalPadding,
+                                end = NormalPadding
+                            ),
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1
+                    )
+                }
             }
 
-            Icon(
-                modifier = Modifier
-                    .padding(start = NormalPadding, top = SmallPadding)
-                    .size(DetailIcon),
-                imageVector = Icons.Filled.MailOutline,
-                tint = Color.Blue,
-                contentDescription = ""
-            )
-            Text(
-                text = movie.numComments.toString(),
-                overflow = TextOverflow.Ellipsis,
-                maxLines = 1,
-                style = MovieDetailItemTextStyle,
-                color = Color.Blue,
-                modifier = Modifier
-                    .padding(start = NormalPadding, top = SmallPadding)
-            )
-            Icon(
-                imageVector = Icons.Outlined.Favorite,
-                contentDescription = "",
-                tint = Color.Red,
-                modifier = Modifier
-                    .padding(start = HighPadding, top = SmallPadding, bottom = SmallPadding)
-                    .size(DetailIcon)
-            )
-            Text(
-                text = movie.numLikes.toString(),
-                overflow = TextOverflow.Ellipsis,
-                maxLines = 1,
-                style = MovieDetailItemTextStyle,
-                color = Color.Red,
-                modifier = Modifier
-                    .padding(start = NormalPadding, top = SmallPadding, bottom = SmallPadding)
-            )
-            Icon(
-                if (movie.isLiked) Icons.Filled.Favorite else Icons.Filled.FavoriteBorder,
-                tint = Color.Red,
-                modifier = Modifier
-                    .padding(NormalPadding),
-                contentDescription = null
-            )
-
+            Row {
+                Icon(
+                    modifier = Modifier
+                        .padding(start = NormalPadding, top = SmallPadding)
+                        .size(DetailIcon),
+                    imageVector = Icons.Filled.MailOutline,
+                    tint = Color.Blue,
+                    contentDescription = ""
+                )
+                Text(
+                    text = movie.numComments.toString(),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    style = MovieDetailItemTextStyle,
+                    color = Color.Blue,
+                    modifier = Modifier
+                        .padding(start = NormalPadding, top = SmallPadding)
+                )
+                Icon(
+                    imageVector = Icons.Outlined.Favorite,
+                    contentDescription = "",
+                    tint = Color.Red,
+                    modifier = Modifier
+                        .padding(start = HighPadding, top = SmallPadding, bottom = SmallPadding)
+                        .size(DetailIcon)
+                )
+                Text(
+                    text = movie.numLikes.toString(),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    style = MovieDetailItemTextStyle,
+                    color = Color.Red,
+                    modifier = Modifier
+                        .padding(start = NormalPadding, top = SmallPadding, bottom = SmallPadding)
+                )
+            }
         }
     }
 }

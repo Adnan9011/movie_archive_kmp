41c1762a4b94e7afc29aee11708d896264aa13ed
Adnan9011 <adnan9011@gmail.com>
2024-01-26 13:16:34 +0330

    feat: Update movie in data layer

--- shared/data/src/commonMain/kotlin/com/moviearchive/data/repository/MovieRepository.kt
+++ shared/data/src/commonMain/kotlin/com/moviearchive/data/repository/MovieRepository.kt
@@ -2,15 +2,10 @@ package com.moviearchive.data.repository
 
 import com.moviearchive.core.Error
 import com.moviearchive.core.Result
-import com.moviearchive.data.model.CelebritiesDataModel
 import com.moviearchive.data.model.MovieDataModel
-import com.moviearchive.data.model.PagingDataModel
-import com.moviearchive.data.model.WeekTopDataModel
 import kotlinx.coroutines.flow.Flow
 
 interface MovieRepository {
     fun get(id: String): Flow<Result<MovieDataModel, Error>>
     fun search(title: String): Flow<Result<List<MovieDataModel>, Error>>
-    fun weekTopTen(): Flow<Result<List<WeekTopDataModel>, Error>>
-    fun popularCelebrities(): Flow<Result<PagingDataModel<CelebritiesDataModel>, Error>>
 }
--- shared/data/src/commonMain/kotlin/com/moviearchive/data/repository/MovieRepositoryImpl.kt
+++ shared/data/src/commonMain/kotlin/com/moviearchive/data/repository/MovieRepositoryImpl.kt
@@ -3,22 +3,15 @@ package com.moviearchive.data.repository
 import com.moviearchive.core.Error
 import com.moviearchive.core.Result
 import com.moviearchive.core.map
-import com.moviearchive.data.model.CelebritiesDataModel
 import com.moviearchive.data.model.MovieDataModel
-import com.moviearchive.data.model.PagingDataModel
-import com.moviearchive.data.model.WeekTopDataModel
 import com.moviearchive.data.model.toData
 import com.moviearchive.data.source.api.api.ApiService
-import com.moviearchive.data.source.datastore.DataStoreSource
-import com.moviearchive.data.source.db.dao.MovieDao
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 
 class MovieRepositoryImpl(
-    val api: ApiService,
-    val dao: MovieDao,
-    val dataStore: DataStoreSource
+    val api: ApiService
 ) : MovieRepository {
     override fun get(id: String): Flow<Result<MovieDataModel, Error>> =
         api.movie(id).map { result ->
@@ -32,20 +25,6 @@ class MovieRepositoryImpl(
     override fun search(title: String): Flow<Result<List<MovieDataModel>, Error>> =
         api.search(title).map { result ->
             result.map { it.data.map { it.toData() } }
-        }.catch { throwable ->
-            Result.Failure(Error(message = throwable.message ?: "", throwable = throwable))
-        }
-
-    override fun weekTopTen(): Flow<Result<List<WeekTopDataModel>, Error>> =
-        api.weekTopTen().map { result ->
-            result.map { it.data.map { it.toData() } }
-        }.catch { throwable ->
-            Result.Failure(Error(message = throwable.message ?: "", throwable = throwable))
-        }
-
-    override fun popularCelebrities(): Flow<Result<PagingDataModel<CelebritiesDataModel>, Error>> =
-        api.popularCelebrities().map { result ->
-            result.map { it.data!!.toData() }
         }.catch { throwable ->
             Result.Failure(Error(message = throwable.message ?: "", throwable = throwable))
         }
